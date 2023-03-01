09ecf82b6cb092881e5d1f3797a18c5bb1b36375
GeoffreyGuez <geoffrey.guez@chekk.me>
2023-03-01 15:33:57 +0100

    Auth page with css

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -24,16 +24,16 @@ const LoginPage = () => {
   };
   
   return (
-    <div>
+    <div id="log-in-container">
       <h1>Login Page</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <TextField id="standard-basic" label="Email" variant="standard"
-         type="text" placeholder="Email" {...register("email")} />
-        {errors.email?.message && <p>{errors.email?.message as string}</p>}
-        <TextField id="standard-basic" label="Password" variant="standard"
-         type="password" placeholder="Password" {...register("password")} />
-        {errors.password?.message && <p>{errors.password?.message as string}</p>}
-        <Button variant="outlined" type="submit">Submit</Button>
+        <TextField id="standard-basic" className="log-in-input" label="Email" variant="standard"
+         type="text" {...register("email")} />
+        {errors.email?.message && <p className='error-message'>{errors.email?.message as string}</p>}
+        <TextField id="standard-basic" className="log-in-input" label="Password" variant="standard"
+         type="password" {...register("password")} />
+        {errors.password?.message && <p className='error-message'>{errors.password?.message as string}</p>}
+        <Button variant="outlined" type="submit" id="log-in-button-submit">Submit</Button>
       </form>
     </div>
   );
