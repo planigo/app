4df3c418a855d8c23bce95bb260d7f9c965c8eba
GeoffreyGuez <geoffrey.guez@chekk.me>
2023-03-01 15:20:31 +0100

    Added front verification on email and password (min length: 8)

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,51 +1,43 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
-import { Button, Autocomplete, TextField, Box } from '@mui/material';
+import { useForm} from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { TextField, Button } from '@mui/material';
+
+const schema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+})
 
 const LoginPage = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const router = useRouter();
+  const { 
+    register, 
+    handleSubmit, 
+    formState: { errors } 
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
 
-  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
+  const onSubmit = (data) => {
+    console.log(data);
   };
-
-  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
-  };
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    // Perform login logic here using email and password
-    router.push('/'); // Redirect to dashboard page after successful login
-  };
-
+  
   return (
     <div>
       <h1>Login Page</h1>
-      <form onSubmit={handleSubmit} style={{display : 'flex', alignItems: 'center'}}>
-        <TextField
-          id="email"
-          label="Email"
-          type="email"
-          autoComplete="current-password"
-          value={email}
-          onChange={handleEmailChange}
-        />
-        <TextField
-          id="password"
-          label="password"
-          type="password"
-          autoComplete="current-password"
-          value={password}
-          onChange={handlePasswordChange}
-          style={{ marginLeft: '10px' }}
-        />
-        <Button variant="outlined" type="submit" style={{marginLeft: '10px' }}>Submit</Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField id="standard-basic" label="Email" variant="standard"
+         type="text" placeholder="Email" {...register("email")} />
+        {errors.email?.message && <p>{errors.email?.message as string}</p>}
+        <TextField id="standard-basic" label="Password" variant="standard"
+         type="password" placeholder="Password" {...register("password")} />
+        {errors.password?.message && <p>{errors.password?.message as string}</p>}
+        <Button variant="outlined" type="submit">Submit</Button>
       </form>
     </div>
   );
 };
 
 export default LoginPage;
+
