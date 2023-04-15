501e665162ac6b6fe2a5eb76187d9d655d3e5a80
Jessy <c.j91350@gmail.com>
2023-04-15 02:38:31 +0200

    feat: consumation of  GetShopsByCategory usecase in v2 app

---
+++ packages/apps/planigo/src/components/ShopCardItem.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+    Button,
+    Card,
+    CardMedia,
+    CardContent,
+    Typography,
+    CardActions,
+} from "@mui/material";
+import Link from "next/link";
+import { Shop } from "@planigo/core/lib/shopping/domain/models/Shop.model";
+
+type ShopCardProps = {
+    shop: Shop;
+};
+
+const ShopCardItem = ({ shop }: ShopCardProps) => {
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+                sx={{ height: 140 }}
+                image="https://media.gqmagazine.fr/photos/5c613df51109c5dfe39752d1/master/w_1920,h_1280,c_limit/6.%20Prestige%20Barber.jpg"
+                title="Image de la boutique"
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {shop.name}
+                </Typography>
+                <Typography gutterBottom variant="body2" component="div">
+                    {shop.description}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Link
+                    href={{
+                        pathname: "/shops/[id]",
+                        query: { id: shop.id },
+                    }}
+                >
+                    <Button variant="outlined">Prendre Rendez-vous</Button>
+                </Link>
+            </CardActions>
+        </Card>
+    );
+};
+
+export default ShopCardItem;
---
+++ packages/apps/planigo/src/pages/search/index.tsx
@@ -0,0 +1,41 @@
+import { useRouter } from 'next/router'
+import { Box, CircularProgress } from "@mui/material";
+import { shoppingService } from "../../services/Shopping.service";
+import { Shop } from "@planigo/core/lib/shopping/domain/models/Shop.model";
+import ShopCardItem from "../../components/ShopCardItem";
+
+const SearchResultPage = () => {
+    const router = useRouter()
+    const shopCategory: string = (router.query.category as string) || ""
+    const { isLoading, data: shopsByCategory } = shoppingService().useGetShopsByCategoryQuery(shopCategory);
+   
+    return (
+        <Box
+            sx={{
+                marginTop: 10,
+                display: "flex",
+                flexDirection: "row",
+                flexWrap: "wrap",
+                justifyContent: "space-around",
+                paddingbottom: 2,
+            }}
+        >
+            {isLoading
+                ? <CircularProgress
+                    color="info"
+                    size="lg"
+                    value={30}
+                />
+                : shopsByCategory ? (
+                    shopsByCategory.map((shop: Shop) => (
+                        <ShopCardItem key={shop.id} shop={shop} />
+                    ))
+                ) : (
+                    <p>Pas de Boutique</p>
+                )
+            }
+        </Box>
+    );
+};
+
+export default SearchResultPage;
